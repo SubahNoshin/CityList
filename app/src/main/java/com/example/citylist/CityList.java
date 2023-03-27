6b1f75771d1991df909c7c865842a4b2278122a0
Subah Noshin <subahnoshin1234@gmail.com>
2023-03-27 21:01:19 +0600

    changes

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,6 +2,7 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -27,9 +28,47 @@ public class CityList {
      * @return
      *      Return the sorted list of cities
      */
-    public List<City> getCities() {
+    public List<City> getCities(int p) {
         List<City> cityList = cities;
-        Collections.sort(cityList);
-        return cityList;
+        if (p == 1) {
+            Collections.sort(cityList);
+
+        } else {
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City city, City t1) {
+                    return city.getProvinceName().compareTo(t1.getProvinceName());
+                }
+            });
+        }
+            return cityList;
+
+    }
+
+    /**
+     * this is used to delete city from list
+     * @param city
+     * checking if the city is in the list or not
+     * if is in the list ,then remove ,else throw exception
+     */
+    public void delete(City city)
+    {
+        if(!cities.contains(city))
+        {
+            throw new IllegalArgumentException();
+        }
+        else
+        {
+            cities.remove(city);
+        }
+    }
+
+    /**
+     *this returns the size of the List
+     * @return the no of cities in the list
+     */
+    public int count()
+    {
+        return cities.size();
     }
 }
